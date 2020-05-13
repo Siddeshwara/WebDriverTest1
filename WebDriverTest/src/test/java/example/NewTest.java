aa26cda41d1243f8e84078544711066748245d1d
Siddeshwara <siddeshwarasgowda@gmail.com>
2020-05-13 16:24:21 +0200

    New project maven test demo.

---
+++ WebDriverTest/src/test/java/example/NewTest.java
@@ -0,0 +1,35 @@
+package example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	private WebDriver driver;
+
+	@Test
+	public void testEasy() {
+		driver.get("http://demo.guru99.com/test/guru99home/");
+		String title = driver.getTitle();
+		Assert.assertTrue(title.contains("Demo Guru99 Page"));
+		System.out.println("Test Pass!");
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+}
